089505834b5d409b94c321e021e94dee4f8dece0
vale-c <valent95@gmail.com>
2021-11-01 09:16:45 +0100

    fixed deploy variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,3 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    amd: true,
-    node: true,
-    es6: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:jsx-a11y/recommended',
-    'prettier',
-    'next',
-    'next/babel',
-    'next/core-web-vitals',
-  ],
-  rules: {
-    'prettier/prettier': 'error',
-    'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['hrefLeft', 'hrefRight'],
-        aspects: ['invalidHref', 'preferButton'],
-      },
-    ],
-    'react/prop-types': 0,
-    'no-unused-vars': 0,
-    'react/no-unescaped-entities': 0,
-  },
+  parser: 'babel-eslint',
 }
